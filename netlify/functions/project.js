7e68d14e62a2fd1b2db2a4f58eb39ddc4e289f69
brock810 <130688727+brock810@users.noreply.github.com>
2024-01-06 13:04:44 -0600

    message

--- netlify/functions/project.js
+++
@@ -1,9 +0,0 @@
-// functions/example.js
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello from the serverless function!" }),
-    };
-  };
-  
-  
