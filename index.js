ee3244e24745f92628bf9d057e69465ba1a7f514
brock810 <130688727+brock810@users.noreply.github.com>
2024-01-06 11:26:06 -0600

    message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8888;
 
 app.use(cors());
 app.use(express.json());
